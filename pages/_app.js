e752679ba1aca29fd8d75b126fb51344a7004e8d
Aryanmallik <aryanmallik2004@gmail.com>
2023-11-16 14:35:08 +0530

    aa

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,23 @@
 // import '../styles/styles.css';
 // import '../styles/global.css';
+import Head from 'next/head';
 import Home from '.'
 
 function MyApp({ Component, pageProps }) {
-  return <div>
-    {/* <Home/> */}
-    <Component {...pageProps} />
-  </div>
+  return (
+    <>
+      <Head>
+        <title>N!gga</title>
+        <meta name="🤓" content="I take photos🤓" />
+      </Head>
+      <div className="app-container">
+        
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default MyApp;
