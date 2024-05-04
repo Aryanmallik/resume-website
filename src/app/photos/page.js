e1d3fa9ce18c3a7c40a3e796b6867ad1cc4c25fa
Aryanmallik <aryanmallik2004@gmail.com>
2024-05-04 11:25:21 +0530

    drive

--- src/app/photos/page.js
+++ src/app/photos/page.js
@@ -1,248 +1,123 @@
-// pages/resume.js
-import React from 'react';
-// import styles from '../styles/styles.css';
-import Header from '../../../components/header copy';
-import Footer from '../../../components/footer';
-import Head from 'next/head';
-export default function Resume(){
-  return (
-    <div>
+'use client'
+import React, { useState, useEffect } from 'react';
+import Footer from '@/components/footer';
+import Headers from "@/components/header";
 
-      <Head>
-  <link rel="shortcut icon" href="/favicon/favicon.ico" />
-    </Head>
+const Resume = () => {
+    const [beachImages, setBeachImages] = useState([]);
+    const [concertImages, setConcertImages] = useState([]);
+    const [natureImages, setNatureImages] = useState([]);
+    const [nightSkyImages, setNightSkyImages] = useState([]);
 
-       <link href="https://cdn.jsdelivr.net/npm/daisyui@3.9.3/dist/full.css" rel="stylesheet" type="text/css" />
-    <script src="https://cdn.tailwindcss.com" />
-<Header/>
-<div className={"bg-black"}>
-  <h1 className={"font-bold"}>Beaches</h1>
-<div className="carousel carousel-center max-w-full p-4 space-x-5  rounded-box">
-    
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/16.jpg" />
-   </div> 
-   </div>
-    </div>
+    useEffect(() => {
+        const fetchImages = async () => {
+            try {
+                const response = await fetch(`https://avanea-f5cf1a1fd40c.herokuapp.com/api/google-drive-images?folderId=1n6JbL_DuqLcMOTlMOLzKqs8j0bby4iYU`);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch images');
+                }
+                const data = await response.json();
+                const beachImagesData = [];
+                const concertImagesData = [];
+                const natureImagesData = [];
+                const nightSkyImagesData = [];
+                data.forEach(image => {
+                    if (image && image.name && image.id) {
+                        const imageUrl = `https://drive.lienuc.com/uc?id=${image.id}`;
+                        if (image.name.toLowerCase().includes('beach')) {
+                            beachImagesData.push({ name: image.name, url: imageUrl });
+                        } else if (image.name.toLowerCase().includes('concert')) {
+                            concertImagesData.push({ name: image.name, url: imageUrl });
+                        } else if (image.name.toLowerCase().includes('nature')) {
+                            natureImagesData.push({ name: image.name, url: imageUrl });
+                        } else if (image.name.toLowerCase().includes('night sky')) {
+                            nightSkyImagesData.push({ name: image.name, url: imageUrl });
+                        }
+                    } else {
+                        console.error('Invalid image data:', image);
+                    }
+                });
+                setBeachImages(beachImagesData);
+                setConcertImages(concertImagesData);
+                setNatureImages(natureImagesData);
+                setNightSkyImages(nightSkyImagesData);
+            } catch (error) {
+                console.error('Error fetching images:', error);
+            }
+        };
 
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/19.jpg" />
-   </div> 
-   </div>
-    </div>
+        fetchImages();
+    }, []);
 
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/18.jpg" />
-   </div> 
-   </div>
-    </div>
+    return (
+        <div>
+            <head><link href="https://cdn.jsdelivr.net/npm/daisyui@3.9.3/dist/full.css" rel="stylesheet" type="text/css" /> </head>
+            <Headers />
 
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/17.jpg" />
-   </div> 
-   </div>
-    </div>
+            <div className={"bg-black"}>
+                <h1 className={"font-bold"}>Beaches</h1>
+                <div className="carousel carousel-center max-w-full p-4 space-x-5 rounded-box">
+                    {beachImages.map((image, index) => (
+                        <div className="carousel-item">
+                            <div className="h-96 carousel carousel-vertical rounded-box">
+                                <div className="carousel-item h-full">
+                                    <img crossOrigin="anonymous" src={image.url} alt={`Beach ${index}`} height={200} />                                
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
 
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/21.1.jpg" alt='beach'/>
-   </div> 
-   </div>
-    </div>
+            <div className={"bg-black"}>
+                <h1 className={"font-bold"}>Concerts</h1>
+                <div className="carousel carousel-center max-w-full p-4 space-x-5 rounded-box">
+                    {concertImages.map((image, index) => (
+                        <div className="carousel-item">
+                            <div className="h-96 carousel carousel-vertical rounded-box">
+                                <div className="carousel-item h-full">
+                                    <img crossOrigin="anonymous" src={image.url} alt={`Concert ${index}`} height={200} />                                
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
 
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/20.jpg" />
-   </div> 
-   </div>
-    </div> 
-</div>
-</div>
+            <div className={"bg-black"}>
+                <h1 className={"font-bold"}>Nature</h1>
+                <div className="carousel carousel-center max-w-full p-4 space-x-5 rounded-box">
+                    {natureImages.map((image, index) => (
+                        <div className="carousel-item">
+                        <div className="h-96 carousel carousel-vertical rounded-box">
+                        <div className="carousel-item h-full">
+                                    <img crossOrigin="anonymous" src={image.url} alt={`Nature ${index}`} height={200}  />                                
+                        </div>
+                        </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
 
-<div className={"bg-black"}>
-  <h1 className={"font-bold"}>Concerts</h1>
-<div className="carousel carousel-center max-w-full p-4 space-x-5  rounded-box">
-    
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/1.jpg" />
-   </div> 
-   </div>
-    </div>
+            <div className={"bg-black"}>
+                <h1 className={"font-bold"}>Night Sky</h1>
+                <div className="carousel carousel-center max-w-full p-4 space-x-5 rounded-box">
+                    {nightSkyImages.map((image, index) => (
+                        <div className="carousel-item">
+                            <div className="h-96 carousel carousel-vertical rounded-box">
+                                <div className="carousel-item h-full">
+                                    <img crossOrigin="anonymous" src={image.url} alt={`Night sky ${index}`} height={200} />                                
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
 
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/2.jpg" />
-   </div> 
-   </div>
-    </div>
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/4.jpg" />
-   </div> 
-   </div>
-    </div>
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/3.jpg" />
-   </div> 
-   </div>
-    </div>
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/6.jpg" />
-   </div> 
-   </div>
-    </div>
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/7.jpg" />
-   </div> 
-   </div>
-    </div>
-
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/5.jpg" />
-   </div> 
-   </div>
-    </div>
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/8.jpg" />
-   </div> 
-   </div>
-    </div>
-
-</div>
-</div>
-
-
-
-<div className={"bg-black"}>
-  <h1 className={"font-bold"}>Nature</h1>
-<div className="carousel carousel-center max-w-full p-4 space-x-5  rounded-box">
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/9.jpg" />
-   </div> 
-   </div>
-    </div>
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/10.jpg" />
-   </div> 
-   </div>
-    </div>
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/11.jpg" />
-   </div> 
-   </div>
-    </div>
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/12.jpg" />
-   </div> 
-   </div>
-    </div>
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/13.jpg" />
-   </div> 
-   </div>
-    </div>
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/15.5.jpg" />
-   </div> 
-   </div>
-    </div>
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/14.jpg" />
-   </div> 
-   </div>
-    </div>
-
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/15.jpg" />
-   </div> 
-   </div>
-    </div>
-
-</div>
-</div>
-
-
-
-<div className={"bg-black"}>
-  <h1 className={"font-bold"}>Night Sky</h1>
-<div className="carousel carousel-center max-w-full p-4 space-x-5  rounded-box">
-    
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/24.jpg" />
-   </div> 
-   </div>
-    </div>
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/23.jpg" />
-   </div> 
-   </div>
-    </div>
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/22.jpg" alt='Night sky' />
-   </div> 
-   </div>
-    </div>
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/25.jpg" />
-   </div> 
-   </div>
-    </div>
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/26.jpg" />
-   </div> 
-   </div>
-    </div>
-
-    <div className="carousel-item">
- <div className="h-96 carousel carousel-vertical rounded-box">
-   <div className="carousel-item h-full"><img src="/27.jpg" />
-   </div> 
-   </div>
-    </div> 
-</div>
-</div>
-<Footer/>
- </div>
-  );
+            <Footer />
+        </div>
+    );
 };
+
+export default Resume;
+;
