9e6cc0c2275e41a23c55ddeeb8cb03e42eb91c4e
Aryanmallik <aryanmallik2004@gmail.com>
2024-05-06 22:00:47 +0530

    about me

--- src/app/page.js
+++ src/app/page.js
@@ -7,88 +7,92 @@ import Footer from "../../components/footer";
 import styles from "../../styles/index.module.css";
 import Head from "next/head";
 const Home = () => {
-  return (
-    <>
+    return (
+        <>
 
-      <link
-        href="https://cdn.jsdelivr.net/npm/daisyui@3.9.3/dist/full.css"
-        rel="stylesheet"
-        type="text/css"
-      />
-      <script src="https://cdn.tailwindcss.com" />
-
-      <Header />
-      <div className={styles.diva}>
-        <div
-          className="hero h-screen"
-          style={{ backgroundImage: 'url("bg.jpg")' }}
-        >
-          <div className="hero-overlay bg-opacity-60"></div>
-
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <div style={{ marginLeft: "5rem" }}>
-              <h1 className="text-5xl font-bold">About Me</h1>
-              <p className="py-6">
-                Hi👋🏻, Im Aryan👦🏻. I take photso📸.
-                <br />
-              </p>
-              <h2>
-                Check Out My{" "}
-                <a href="https://www.youtube.com/AryanMallik" target="_blank">
-                  <b>
-                    <u>YouTube channel</u>
-                  </b>
-                </a>
-              </h2>
-              <br />
-              <Link href={"photos"} className="btn glass">
-                My Photos
-              </Link>
-            </div>
-            <img
-              src="/pic.jpg"
-              className=" rounded-lg shadow-2xl"
-              style={{ width: "30rem" }}
+            <link
+                href="https://cdn.jsdelivr.net/npm/daisyui@3.9.3/dist/full.css"
+                rel="stylesheet"
+                type="text/css"
             />
-          </div>
-        </div>
-      </div>
+            <script src="https://cdn.tailwindcss.com" />
 
-      <div className={styles.divmobile}>
-        <div className="hero h-screen" style={{ backgroundColor: "black" }}>
-          <div className="hero-overlay bg-opacity-60"></div>
+            <Header />
+            <div className={styles.diva}>
+                <div
+                    className="hero h-screen"
+                    style={{ backgroundImage: 'url("bg.jpg")' }}
+                >
+                    <div className="hero-overlay bg-opacity-60"></div>
 
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <img
-              src="/pic.jpg"
-              className=" rounded-lg shadow-2xl"
-              style={{ width: "30rem" }}
-            />
-            <div style={{ marginLeft: "5rem" }}>
-              <h1 className="text-5xl font-bold">About Me</h1>
-              <p className="py-6">
-                Hi👋🏻, Im Aryan👦🏻. I take photso📸. my camera📸 is nikon D5300🤢.
-                <br />
-              </p>
-              <h2>
-                Check Out My{" "}
-                <a href="https://www.youtube.com/AryanMallik" target="_blank">
-                  <b>
-                    <u>YouTube channel</u>
-                  </b>
-                </a>
-              </h2>
-              <br />
-              <Link className="btn glass" href="/photos">
-                My Photos
-              </Link>
+                    <div className="hero-content flex-col lg:flex-row-reverse">
+                        <div style={{ marginLeft: "5rem" }}>
+                            <div className={styles.wrapper}>
+                                <div className={styles.typingdemo}>
+                                    About Me...
+                                </div>
+                            </div>
+                            <p className="py-6">
+                                Hi👋🏻, Im Aryan👦🏻. I take photso📸.
+                                <br />
+                            </p>
+                            <h2>
+                                Check Out My{" "}
+                                <a href="https://www.youtube.com/AryanMallik" target="_blank">
+                                    <b>
+                                        <u>YouTube channel</u>
+                                    </b>
+                                </a>
+                            </h2>
+                            <br />
+                            <Link href={"photos"} className="btn glass">
+                                My Photos
+                            </Link>
+                        </div>
+                        <img
+                            src="/pic.jpg"
+                            className=" rounded-lg shadow-2xl"
+                            style={{ width: "30rem" }}
+                        />
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
+
+            <div className={styles.divmobile}>
+                <div className="hero h-screen" style={{ backgroundColor: "black" }}>
+                    <div className="hero-overlay bg-opacity-60"></div>
+
+                    <div className="hero-content flex-col lg:flex-row-reverse">
+                        <img
+                            src="/pic.jpg"
+                            className=" rounded-lg shadow-2xl"
+                            style={{ width: "30rem" }}
+                        />
+                        <div style={{ marginLeft: "5rem" }}>
+                            <h1 className="text-5xl font-bold">About Me</h1>
+                            <p className="py-6">
+                                Hi👋🏻, Im Aryan👦🏻. I take photso📸. my camera📸 is nikon D5300🤢.
+                                <br />
+                            </p>
+                            <h2>
+                                Check Out My{" "}
+                                <a href="https://www.youtube.com/AryanMallik" target="_blank">
+                                    <b>
+                                        <u>YouTube channel</u>
+                                    </b>
+                                </a>
+                            </h2>
+                            <br />
+                            <Link className="btn glass" href="/photos">
+                                My Photos
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
 };
 
 export default Home;
