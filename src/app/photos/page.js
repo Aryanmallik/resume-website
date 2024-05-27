6260a7fb92601da49a508c5c15b2c5827e225790
Aryanmallik <aryanmallik2004@gmail.com>
2024-05-27 15:28:03 +0530

    photos

--- src/app/photos/page.js
+++ src/app/photos/page.js
@@ -62,108 +62,108 @@ const Resume = () => {
 
     return (
         <div>
-            <Header />
-            <div className="bg-black">
-                <h1 className="font-bold text-white">Beaches</h1>
-                <div className="carousel carousel-center p-4 max-w-full space-x-5 rounded-box">
-                    {beachImages.map((image, index) => (
-                        <div className="carousel-item" key={index}>
-                            <div className="h-48 sm:h-96 carousel carousel-vertical rounded-box">
-                                <div className="h-full">
-                                    <img
-                                        crossOrigin="anonymous"
-                                        src={image.url}
-                                        alt={`Beach ${index}`}
-                                        className="w-full h-full object-cover cursor-pointer"
-                                        onClick={() => openModal(image.url)}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+        <Header />
+        <div className="bg-black">
+        <h1 className="font-bold text-white">Beaches</h1>
+        <div className="carousel carousel-center p-4 max-w-full space-x-5 rounded-box">
+        {beachImages.map((image, index) => (
+            <div className="carousel-item" key={index}>
+            <div className="h-48 sm:h-96 carousel carousel-vertical rounded-box">
+            <div className="h-full">
+            <img
+            crossOrigin="anonymous"
+            src={image.url}
+            alt={`Beach ${index}`}
+            className="w-full h-full object-cover cursor-pointer"
+            onClick={() => openModal(image.url)}
+            />
             </div>
-
-            <div className="bg-black">
-                <h1 className="font-bold text-white">Concerts</h1>
-                <div className="carousel carousel-center max-w-full p-4 space-x-5 rounded-box">
-                    {concertImages.map((image, index) => (
-                        <div className="carousel-item" key={index}>
-                            <div className="h-48 sm:h-96 carousel carousel-vertical rounded-box">
-                                <div className="h-full">
-                                    <img
-                                        crossOrigin="anonymous"
-                                        src={image.url}
-                                        alt={`Concert ${index}`}
-                                        className="w-full h-full object-cover cursor-pointer"
-                                        onClick={() => openModal(image.url)}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
             </div>
-
-            <div className="bg-black">
-                <h1 className="font-bold text-white">Nature</h1>
-                <div className="carousel carousel-center max-w-full p-4 space-x-5 rounded-box">
-                    {natureImages.map((image, index) => (
-                        <div className="carousel-item" key={index}>
-                            <div className="h-48 sm:h-96 carousel carousel-vertical rounded-box">
-                                <div className="h-full">
-                                    <img
-                                        crossOrigin="anonymous"
-                                        src={image.url}
-                                        alt={`Nature ${index}`}
-                                        className="w-full h-full object-cover cursor-pointer"
-                                        onClick={() => openModal(image.url)}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
             </div>
+        ))}
+        </div>
+        </div>
 
-            <div className="bg-black">
-                <h1 className="font-bold text-white">Night Sky</h1>
-                <div className="carousel carousel-center max-w-full p-4 space-x-5 rounded-box">
-                    {nightSkyImages.map((image, index) => (
-                        <div className="carousel-item" key={index}>
-                            <div className="h-48 sm:h-96 carousel carousel-vertical rounded-box">
-                                <div className="h-full">
-                                    <img
-                                        crossOrigin="anonymous"
-                                        src={image.url}
-                                        alt={`Night sky ${index}`}
-                                        className="w-full h-full object-cover cursor-pointer"
-                                        onClick={() => openModal(image.url)}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+        <div className="bg-black">
+        <h1 className="font-bold text-white">Night Sky</h1>
+        <div className="carousel carousel-center max-w-full p-4 space-x-5 rounded-box">
+        {nightSkyImages.map((image, index) => (
+            <div className="carousel-item" key={index}>
+            <div className="h-48 sm:h-96 carousel carousel-vertical rounded-box">
+            <div className="h-full">
+            <img
+            crossOrigin="anonymous"
+            src={image.url}
+            alt={`Night sky ${index}`}
+            className="w-full h-full object-cover cursor-pointer"
+            onClick={() => openModal(image.url)}
+            />
             </div>
+            </div>
+            </div>
+        ))}
+        </div>
+        </div>
 
-            {selectedImageUrl && (
-                <div
-                    className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
-                    onClick={closeModal}
-                >
-                    <div className="relative max-w-screen-lg mx-auto rounded-xl overflow-hidden">
-                        <img
-                            crossOrigin="anonymous"
-                            src={selectedImageUrl}
-                            alt="Selected"
-                            className="max-w-full max-h-screen-3/4 object-contain"
-                        />
-                    </div>
-                </div>
-            )}
+        <div className="bg-black">
+        <h1 className="font-bold text-white">Nature</h1>
+        <div className="carousel carousel-center max-w-full p-4 space-x-5 rounded-box">
+        {natureImages.map((image, index) => (
+            <div className="carousel-item" key={index}>
+            <div className="h-48 sm:h-96 carousel carousel-vertical rounded-box">
+            <div className="h-full">
+            <img
+            crossOrigin="anonymous"
+            src={image.url}
+            alt={`Nature ${index}`}
+            className="w-full h-full object-cover cursor-pointer"
+            onClick={() => openModal(image.url)}
+            />
+            </div>
+            </div>
+            </div>
+        ))}
+        </div>
+        </div>
+        <div className="bg-black">
+        <h1 className="font-bold text-white">Concerts</h1>
+        <div className="carousel carousel-center max-w-full p-4 space-x-5 rounded-box">
+        {concertImages.map((image, index) => (
+            <div className="carousel-item" key={index}>
+            <div className="h-48 sm:h-96 carousel carousel-vertical rounded-box">
+            <div className="h-full">
+            <img
+            crossOrigin="anonymous"
+            src={image.url}
+            alt={`Concert ${index}`}
+            className="w-full h-full object-cover cursor-pointer"
+            onClick={() => openModal(image.url)}
+            />
+            </div>
+            </div>
+            </div>
+        ))}
+        </div>
+        </div>
 
-            <Footer />
+
+        {selectedImageUrl && (
+            <div
+            className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
+            onClick={closeModal}
+            >
+            <div className="relative max-w-screen-lg mx-auto rounded-xl overflow-hidden">
+            <img
+            crossOrigin="anonymous"
+            src={selectedImageUrl}
+            alt="Selected"
+            className="max-w-full max-h-screen-3/4 object-contain"
+            />
+            </div>
+            </div>
+        )}
+
+        <Footer />
         </div>
     );
 };
