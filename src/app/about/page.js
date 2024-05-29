d88eae7233b90bc8ae82a652bfea7399a5fe0784
Aryanmallik <aryanmallik2004@gmail.com>
2024-05-29 23:10:04 +0530

    title

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -9,6 +9,24 @@ const About = () => {
 
     useEffect(() => {
         document.title = "About Me";
+        const ogTitle = "About Me";
+        const ogDescription = "Explore my experiences, achievements, and aspirations as I navigate through life's adventures.";
+        const ogImage = "./about1.jpg";
+
+        const existingOgTitle = document.querySelector('meta[property="og:title"]');
+        if (existingOgTitle) {
+            existingOgTitle.setAttribute('content', ogTitle);
+        }
+
+        const existingOgDescription = document.querySelector('meta[property="og:description"]');
+        if (existingOgDescription) {
+            existingOgDescription.setAttribute('content', ogDescription);
+        }
+
+        const existingOgImage = document.querySelector('meta[property="og:image"]');
+        if (existingOgImage) {
+            existingOgImage.setAttribute('content', ogImage);
+        }
         const handleScroll = () => {
             if (window.scrollY >= 1398 && h1Ref.current) {
                 h1Ref.current.classList.add(styles.shine);
@@ -28,7 +46,7 @@ const About = () => {
     }, []);
     return (
         <>
-        <Header/>
+            <Header />
             <div className={styles.container}>
                 <div className={styles.animatedtitle}>
                     <div className={styles.texttop}>
@@ -45,60 +63,60 @@ const About = () => {
             <ScrollDownArrow />
             <div className={styles.hero}>
                 <div className={styles.text}>
-        <p>
-        As a dedicated Computer Science student at PES University, I am deeply passionate about leveraging my skills and knowledge in web development to contribute to innovative and impactful projects. Over the course of my studies, I have gained proficiency in a variety of programming languages including C, Python, JavaScript, and HTML. My expertise in these languages has enabled me to adeptly translate complex concepts into practical and effective solutions. I am constantly striving to expand my skill set and stay abreast of the latest technological advancements. You can explore some of my projects and see my progress on my <Link style={{ color:"red"}} href="https://github.com/aryanmallik">GitHub</Link>, where I showcase my commitment to continuous learning and development.
+                    <p>
+                        As a dedicated Computer Science student at PES University, I am deeply passionate about leveraging my skills and knowledge in web development to contribute to innovative and impactful projects. Over the course of my studies, I have gained proficiency in a variety of programming languages including C, Python, JavaScript, and HTML. My expertise in these languages has enabled me to adeptly translate complex concepts into practical and effective solutions. I am constantly striving to expand my skill set and stay abreast of the latest technological advancements. You can explore some of my projects and see my progress on my <Link style={{ color: "red" }} href="https://github.com/aryanmallik">GitHub</Link>, where I showcase my commitment to continuous learning and development.
                     </p>
                 </div>
                 <div className={styles.picDiv}>
                     <img className={styles.pic} src="./about1.jpg" />
                 </div>
             </div>
-        <div className={styles.education}>
+            <div className={styles.education}>
                 <h1>
-                   Education 
+                    Education
                 </h1>
-        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
-  <li>
-    <div className="timeline-middle">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
-    </div>
-    <div className="timeline-start md:text-end mb-10">
-      <time className="font-mono italic">2018-2020</time>
-      <div className="text-lg font-black"><Link style={{ color:"red"}}target="_blank" href="https://south.dpsbangalore.edu.in/">Delhi Public School Bangalore South</Link></div>
-        Completed my secondary education at DPS<br/> 
-        Activites: Student Representative · MUN · Scouts · Stand-Up Comedy <br/> Photography · Football
-    </div>
-    <hr/>
-  </li>
-  <li>
-    <hr />
-    <div className="timeline-middle">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
-    </div>
-    <div className="timeline-end mb-10">
-      <time className="font-mono italic">2020-2022</time>
-      <div className="text-lg font-black"><Link style={{ color:"red"}}target="_blank" href="https://south.dpsbangalore.edu.in/">Delhi Public School</Link></div>
-        Completed my upper secondary education at DPS<br/> 
-        PCMC<br/>
-        Skills: Python ·  SQL · C (basics)
-    </div>
-    <hr />
-  </li>
-  <li>
-    <hr />
-    <div className="timeline-middle">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
-    </div>
-    <div className="timeline-start md:text-end mb-10">
-      <time className="font-mono italic">2022-2026</time>
-      <div className="text-lg font-black"><Link style={{ color:"red"}} href="https://pes.edu/" target="_blank">PES University Electronic City</Link></div>
-        Persuing B.Tech CSE at PES.<br/>
-        Skills: Next.js · React.js · Data Structures · C (Programming Language) <br/> Python (Programming Language) · Algorithm Design ·  Node.js · HTML <br/> Cascading Style Sheets (CSS)
-    </div>
-    <hr />
-  </li>
-</ul>
-        </div>
+                <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+                    <li>
+                        <div className="timeline-middle">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                        </div>
+                        <div className="timeline-start md:text-end mb-10">
+                            <time className="font-mono italic">2018-2020</time>
+                            <div className="text-lg font-black"><Link style={{ color: "red" }} target="_blank" href="https://south.dpsbangalore.edu.in/">Delhi Public School Bangalore South</Link></div>
+                            Completed my secondary education at DPS<br />
+                            Activites: Student Representative · MUN · Scouts · Stand-Up Comedy <br /> Photography · Football
+                        </div>
+                        <hr />
+                    </li>
+                    <li>
+                        <hr />
+                        <div className="timeline-middle">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                        </div>
+                        <div className="timeline-end mb-10">
+                            <time className="font-mono italic">2020-2022</time>
+                            <div className="text-lg font-black"><Link style={{ color: "red" }} target="_blank" href="https://south.dpsbangalore.edu.in/">Delhi Public School</Link></div>
+                            Completed my upper secondary education at DPS<br />
+                            PCMC<br />
+                            Skills: Python ·  SQL · C (basics)
+                        </div>
+                        <hr />
+                    </li>
+                    <li>
+                        <hr />
+                        <div className="timeline-middle">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                        </div>
+                        <div className="timeline-start md:text-end mb-10">
+                            <time className="font-mono italic">2022-2026</time>
+                            <div className="text-lg font-black"><Link style={{ color: "red" }} href="https://pes.edu/" target="_blank">PES University Electronic City</Link></div>
+                            Persuing B.Tech CSE at PES.<br />
+                            Skills: Next.js · React.js · Data Structures · C (Programming Language) <br /> Python (Programming Language) · Algorithm Design ·  Node.js · HTML <br /> Cascading Style Sheets (CSS)
+                        </div>
+                        <hr />
+                    </li>
+                </ul>
+            </div>
             <div className={styles.projectContainer}>
                 <h1 ref={h1Ref}>
                     Projects
