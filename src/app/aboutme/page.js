10ab701e11a31ed8b9ade93572e71d9773dda037
Aryanmallik <aryanmallik2004@gmail.com>
2024-05-29 18:41:05 +0530

    about time

--- src/app/aboutme/page.js
+++ src/app/aboutme/page.js
@@ -1,7 +1,30 @@
+'use client'
 import styles from "@/src/styles/aboutme.module.scss";
 import ScrollDownArrow from "@/src/components/scroll";
 import Header from "@/src/components/header";
+import React, { useRef, useEffect } from 'react';
+import Link from "next/link";
 const About = () => {
+    const h1Ref = useRef(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY >= 1398 && h1Ref.current) {
+                h1Ref.current.classList.add(styles.shine);
+                h1Ref.current.style.opacity = '1';
+                window.removeEventListener('scroll', handleScroll);
+                setTimeout(() => {
+                    h1Ref.current.style.backgroundImage = "linear-gradient(90deg, #fff, #fff, #fff)";
+                }, 4900);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return (
         <>
         <Header/>
@@ -9,28 +32,80 @@ const About = () => {
                 <div className={styles.animatedtitle}>
                     <div className={styles.texttop}>
                         <div className={styles.innerText}>
-                            <span className={styles.spanner}>uhhh</span>
-                            <span>ummm</span>
+                            <span className={styles.spanner}>Get</span>
+                            <span>to</span>
                         </div>
                     </div>
                     <div className={styles.textbottom}>
-                        <div className={styles.innerText}>about me!!</div>
+                        <div className={styles.innerText}><p style={{ color: "red" }}>know</p>me</div>
                     </div>
                 </div>
+            </div>
+            <ScrollDownArrow />
+            <div className={styles.hero}>
+                <div className={styles.text}>
+        <p>
+        As a dedicated Computer Science student at PES University, I am deeply passionate about leveraging my skills and knowledge in web development to contribute to innovative and impactful projects. Over the course of my studies, I have gained proficiency in a variety of programming languages including C, Python, JavaScript, and HTML. My expertise in these languages has enabled me to adeptly translate complex concepts into practical and effective solutions. I am constantly striving to expand my skill set and stay abreast of the latest technological advancements. You can explore some of my projects and see my progress on my <Link style={{ color:"red"}} href="https://github.com/aryanmallik">GitHub</Link>, where I showcase my commitment to continuous learning and development.
+                    </p>
+                </div>
+                <div className={styles.picDiv}>
+                    <img className={styles.pic} src="./about1.jpg" />
+                </div>
+            </div>
+        <div className={styles.education}>
+                <h1>
+                   Education 
+                </h1>
+        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+  <li>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <div className="timeline-start md:text-end mb-10">
+      <time className="font-mono italic">2018-2020</time>
+      <div className="text-lg font-black"><Link style={{ color:"red"}}target="_blank" href="https://south.dpsbangalore.edu.in/">Delhi Public School Bangalore South</Link></div>
+        Completed my secondary education at DPS<br/> 
+        Activites: Student Representative · MUN · Scouts · Stand-Up Comedy <br/> Photography · Football
+    </div>
+    <hr/>
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <div className="timeline-end mb-10">
+      <time className="font-mono italic">2020-2022</time>
+      <div className="text-lg font-black"><Link style={{ color:"red"}}target="_blank" href="https://south.dpsbangalore.edu.in/">Delhi Public School</Link></div>
+        Completed my upper secondary education at DPS<br/> 
+        PCMC<br/>
+        Skills: Python·  SQL · C (basics)
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <div className="timeline-start md:text-end mb-10">
+      <time className="font-mono italic">2022-2026</time>
+      <div className="text-lg font-black"><Link style={{ color:"red"}} href="https://pes.edu/" target="_blank">PES University Electronic City</Link></div>
+        Persuing B.Tech CSE at PES.<br/>
+        Skills: Next.js · React.js · Data Structures · C (Programming Language) <br/> Python (Programming Language) · Algorithm Design ·  Node.js · HTML <br/> Cascading Style Sheets (CSS)
+    </div>
+    <hr />
+  </li>
+</ul>
         </div>
-        <ScrollDownArrow/>
-                <div className={styles.hero}>
-                    <div className={styles.text}>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a ligula pretium, consequat ligula ac, tempor nulla. Cras mollis augue sapien, non cursus est lacinia in. Maecenas iaculis neque nisl, quis congue nibh rutrum eget. Curabitur porta felis id fringilla elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque feugiat felis tincidunt justo posuere vehicula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-        </p>
-                    </div>
-                    <div className={styles.picDiv}>
-                        <img className={styles.pic} src="./about1.jpg" />
-                    </div>
-                </div>
-            </>
-            );
+            <div className={styles.projectContainer}>
+                <h1 ref={h1Ref}>
+                    Projects
+                </h1>
+            </div>
+        </>
+    );
 };
 
-            export default About;
+export default About;
 
