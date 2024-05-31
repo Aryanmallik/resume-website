1f362e9a682705cd0e230d0c2d52998a03a39262
Aryanmallik <aryanmallik2004@gmail.com>
2024-05-31 15:00:10 +0530

    himm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,22 +11,32 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <head><meta name="google-site-verification" content="UHkTP1PKasHo-DdxCFLDFX6ENef_XUjgoVmavk0bMTg" />
-            <meta property="og:title" content="Aryan"/>
-                <meta property="og:description" content="Explore My Portfolio: Discover My Projects, Photos, About Me, and Connect on Social MediaExplore My Portfolio: Discover My Projects, Photos, About Me, and Connect on Social Media"/>
-                        <meta property="og:image" content="/about1.jpg"/>
-                        </head>
-                        <body>
-                            <link rel="icon" type="image/png" href="/img/LOgo.png" />
-                            <div>{children}</div>
-                            <div className="stars"> </div>
-                            <div className="stars-duplicate"> </div>
-                            <div className="stars2"> </div>
-                            <div className="stars2-duplicate"> </div>
-                            <div className="stars3"> </div>
-                            <div className="stars3-duplicate"> </div>
-                        </body>
-                    </html>
-                    )
+      <head>
+        <meta name="google-site-verification" content="UHkTP1PKasHo-DdxCFLDFX6ENef_XUjgoVmavk0bMTg" />
+        <meta property="og:title" content="Aryan" />
+        <meta property="og:description" content="Explore My Portfolio: Discover My Projects, Photos, About Me, and Connect on Social Media" />
+        <meta property="og:image" content="/about1.jpg" />
+        <link rel="icon" type="image/png" href="/img/LOgo.png" />
+      </head>
+      <body>
+        <svg
+          viewBox="0 0 640 512"
+          width="100%"
+          height="auto"
+          className="progress"
+        >
+          <path
+            d="M544 224l-128-16-48-16h-24L227.158 44h39.509C278.333 44 288 41.375 288 38s-9.667-6-21.333-6H152v12h16v164h-48l-66.667-80H18.667L8 138.667V208h8v16h48v2.666l-64 8v42.667l64 8V288H16v16H8v69.333L18.667 384h34.667L120 304h48v164h-16v12h114.667c11.667 0 21.333-2.625 21.333-6s-9.667-6-21.333-6h-39.509L344 320h24l48-16 128-16c96-21.333 96-26.583 96-32 0-5.417 0-10.667-96-32z"
+            fill="currentColor"
+          />
+        </svg>
+        <div className="stars"></div>
+        <div className="stars2"></div>
+        <div className="stars3"></div>
+        <div>{children}</div>
+      </body>
+    </html>
+
+    )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,8 @@
 import Hero from '../components/Hero';
+import styles from "../styles/index.module.css"
 export default function Home() {
     return (
-        <div>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap"
-                    rel="stylesheet"
-                />
+        <div className={styles.bod}>
             <Hero />
         </div>
     );
