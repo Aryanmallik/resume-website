9f8488e8b10975ac21a46f887f0293c293982e8d
Aryanmallik <aryanmallik2004@gmail.com>
2024-06-07 21:49:15 +0530

    scroll index

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,16 @@
+'use client'
+import { useEffect } from 'react';
 import Hero from '../components/Hero';
-import styles from "../styles/index.module.css"
+import styles from "../styles/index.module.css";
+
 export default function Home() {
+    useEffect(() => {
+        document.body.classList.add(styles.hiddenOverflow);
+        return () => {
+            document.body.classList.remove(styles.hiddenOverflow);
+        };
+    }, []);
+
     return (
         <div className={styles.bod}>
             <Hero />
